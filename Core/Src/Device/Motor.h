cd29d2253bec303a77d44bddf1aeab171548487e
liu200806317 <40073772+liu200806317@users.noreply.github.com>
2021-03-25 19:06:26 -0600

    Bailiang_2

---
+++ Core/Src/Device/Motor.h
@@ -0,0 +1,13 @@
+/*
+ * Motor.h
+ *
+ *  Created on: Mar. 25, 2021
+ *      Author: Bailiang's Desktop
+ */
+
+#ifndef SRC_DEVICE_MOTOR_H_
+#define SRC_DEVICE_MOTOR_H_
+
+
+
+#endif /* SRC_DEVICE_MOTOR_H_ */
