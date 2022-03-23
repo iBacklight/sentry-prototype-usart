d77ef83ed5486a734217db5f962c5d5dbad00973
iBacklight <alex.haoranqi@gmail.com>
2022-03-23 15:17:21 -0600

    upload sentry chassis for dual motors

--- Core/Src/Applications/Timer_App.c
+++ Core/Src/Applications/Timer_App.c
@@ -12,22 +12,29 @@ extern UART_HandleTypeDef huart7;
 
 void Timer_Task_Func(void const * argument){
 
-uint32_t tick;
+	uint32_t tick;
+
+	char *pdata; // temp data packet buffer from computer
+	char *yaw;
+	yaw = malloc (4);
+	pdata = malloc(PACKLEN+1);
+
 	for (;;){
 		// UART catcher
-		 char *pdata; // temp data packet buffer from computer
-		 char *yaw;
+
 		 // Fix here: may add a switch or what to determine what data needed
 		 int pos = 6; // yaw pos
-		 yaw = malloc (4);
-		 pdata = malloc(7); // packet total size, referring to comm protocol
+		 // packet total size, referring to comm protocol
 
-		 if (HAL_UART_Receive(&huart7, (char*)pdata, 7, HAL_MAX_DELAY) == HAL_OK){
+		 if (HAL_UART_Receive(&huart7, (char*)pdata, (PACKLEN+1), HAL_MAX_DELAY) == HAL_OK){
 			  HAL_GPIO_TogglePin(GPIOG, LD_H_Pin);
-			  comm_pack.yaw_data = parse_pack(pdata, yaw, pos);
+			  //comm_pack.yaw_data = parse_pack_indv(pdata, yaw, pos);
+			  comm_pack = parse_pack_string(pdata);
+			  if (comm_pack.pack_cond == PACKCOR) //&& comm_pack.pitch_data == 5678 && comm_pack.fire_cmd == 0){
+			  		 HAL_GPIO_WritePin(GPIOG, LD_C_Pin, RESET);
 		 }
-		 if (comm_pack.yaw_data == 1234){
-		 		HAL_GPIO_TogglePin(GPIOG, LD_B_Pin);
+		 if (comm_pack.yaw_data == 1234) //&& comm_pack.pitch_data == 5678 && comm_pack.fire_cmd == 0){
+			 HAL_GPIO_WritePin(GPIOG, LD_B_Pin, RESET);
 		  }
 
 		 // HAL_GPIO_WritePin(GPIOG, LD_B_Pin, GPIO_PIN_RESET);
@@ -41,8 +48,8 @@ uint32_t tick;
 		//printf("Current angle (6020): %d \n", temp_buffer.motor_feedback.rx_angle);
 		//printf("Current RPM (3508): %d \n", temp_buffer_3508.motor_feedback.rx_rpm);
 
-		  free(yaw);
-		  free(pdata);
+		 free(yaw);
+		 free(pdata);
 		  /* USER CODE END Gimbal_Task_Function */
 
 		//Timer task
@@ -60,20 +67,18 @@ uint32_t tick;
 		//Send data
 
 		osDelay(1);
-
-	}
 }
 
 /*
- * Func name: parse_pack
- * Use: parse the packages sent from computer and output the motor data
- * Parameter: @ pack: the package received from UART
- * 			  @ parse_data: corresponding data variable, could be yaw, pitch, etc
- * 			  @ pos: The position of the last byte of the currently extracted data
- * Return:
- * Author: Haoran Qi, Created on: Jan, 2022
+ * @ Func name: parse_pack_indv
+ * @ Use: parse the packages sent from computer and output the motor data based on data pos
+ * @ Parameter:  pack: the package received from UART
+ * 			     parse_data: corresponding data variable, could be yaw, pitch, etc
+ * 			     pos: The position of the last byte of the currently extracted data
+ * @ Return:
+ * @ Author: Haoran Qi, Created on: Jan, 2022
  */
-int32_t parse_pack(char* pack, char* parse_data, int pos){
+int32_t parse_pack_indv(char* pack, char* parse_data, int pos){
 
     char pdata[(strlen(pack)+1)]; //pack content size + '\0'
     int32_t data = 0;
@@ -92,7 +97,100 @@ int32_t parse_pack(char* pack, char* parse_data, int pos){
 		osDelay(1);
 	}
 
-
     //data++; // plus 1 to ensure the correct output
     return data;
+}
+
+/*
+ * @ Func name: parse_pack_string
+ * @ Use: parse the packages sent from computer and output the motor data based on pcak data itself.
+ * @ Parameter:
+ * 		pack: the package received from UART
+ *
+ * @ Return:
+ * @ Author: Wei, Shitang, Haoran, Created on: Feb, 2022
+ */
+comm_rx_info parse_pack_string(char* pack)
+{
+	comm_rx_info Sentry_Pack;
+    Sentry_Pack.yaw_data = 0;
+    Sentry_Pack.pitch_data = 0;
+    Sentry_Pack.dist_data = 0;
+    Sentry_Pack.fire_cmd = 0;
+    Sentry_Pack.target_num = 0;
+
+    int position = 6;
+    unsigned char data;
+    int power;
+
+    if (strlen(pack) == PACKLEN)
+    {
+        if (pack[0] == 0x41)
+        {
+            for (int i = 2; i< PACKLEN-1; i++)
+            {
+                if(pack[i]>='0' && pack[i] <= '9')
+                {
+                    Sentry_Pack.pack_cond = PACKCOR;
+                }
+                else
+                {
+                    Sentry_Pack.pack_cond = PACKERR;
+                    HAL_GPIO_WritePin(GPIOG, LD_D_Pin, RESET);
+                    break;
+                }
+            }
+                if(pack[PACKLEN-1]=='0' || pack[PACKLEN-1] == '1') {
+                    Sentry_Pack.pack_cond = PACKCOR;
+                }
+                else
+                {
+                    Sentry_Pack.pack_cond = PACKERR;
+                    HAL_GPIO_WritePin(GPIOG, LD_E_Pin, RESET);
+                }
+            if(Sentry_Pack.pack_cond == PACKCOR)
+            {
+                for(int i=0;i<DATALEN;i++)
+                {
+                    data = pack[position-i-1] - '0';
+                    power = ((int)data)*(pow(10,i));
+                    Sentry_Pack.yaw_data += ((int)data)*(pow(10,i));
+                }
+                position = position + DATALEN;
+
+                for(int i=0;i<DATALEN;i++)
+                {
+                    data = pack[position-i-1] - '0';
+                    Sentry_Pack.pitch_data += ((int)data)*(pow(10,i));
+                }
+                position = position + DATALEN;
+
+                for(int i=0;i<DATALEN;i++)
+                {
+                    data = pack[position-i-1] - '0';
+                    Sentry_Pack.dist_data += ((int)data)*(pow(10,i));
+                }
+                position = position + STATELEN;
+
+                data = pack[position - 1] - '0';
+                Sentry_Pack.target_num = ((int) data);
+
+                position = position + STATELEN;
+
+                data = pack[position - 1] - '0';
+                Sentry_Pack.fire_cmd = ((int) data);
+            }
+        }
+        else
+        {
+            Sentry_Pack.pack_cond = PACKERR;
+            HAL_GPIO_WritePin(GPIOG, LD_F_Pin, RESET);
+        }
+    }
+    else
+    {
+        Sentry_Pack.pack_cond = PACKERR;
+        HAL_GPIO_WritePin(GPIOG, LD_G_Pin, RESET);
+    }
+    return Sentry_Pack;
 }
--- Core/Src/Applications/Timer_App.h
+++ Core/Src/Applications/Timer_App.h
@@ -14,15 +14,24 @@
 #include "can.h"
 #include "../Device/Motor.h"
 
+#define PACKLEN  16
+#define DATALEN  4
+#define STATELEN 1
+#define PACKCOR 0
+#define PACKERR 1
+
 typedef struct {
 	int32_t yaw_data;
 	int32_t pitch_data;
 	int32_t dist_data;
+	int32_t target_num;
 	int32_t fire_cmd;
+	int32_t pack_cond;
 }comm_rx_info;
 
 comm_rx_info comm_pack;
 void Timer_Task_Func(void const * argument);
-int32_t parse_pack(char* pack, char* parse_data, int pos);
+int32_t parse_pack_indv(char* pack, char* parse_data, int pos);
+comm_rx_info parse_pack_string(char* pack);
 
 #endif /* SRC_APPLICATIONS_TIMER_APP_H_ */
